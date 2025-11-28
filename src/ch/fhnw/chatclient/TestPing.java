40be499755878bd68dd7ba8b402aa5f8d7e52545
Alex <alexander.burri@students.fhnw.ch>
2025-11-28 11:29:14 +0100

    maven update

--- src/ch/fhnw/chatclient/TestPing.java
+++
@@ -1,9 +0,0 @@
-package ch.fhnw.chatclient;
-
-public class TestPing {
-    public static void main(String[] args) {
-        ApiClient api = new ApiClient();
-        String response = api.ping();
-        System.out.println("Ping response: " + response);
-    }
-}
